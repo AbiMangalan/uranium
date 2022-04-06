2a4228cb76b66c52b8c36c27d97fc0e1568d74dc
AbiMangalan <86941986+AbiMangalan@users.noreply.github.com>
2022-04-06 22:30:07 +0530

    helper.js

--- src/util/helper.js
+++ src/util/helper.js
@@ -1,10 +1,28 @@
 let printDate = function()
                 {
-                    return "06-04-2022";
+                    let today = new Date();
+                    let date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+                    return date;
                 };
 let printMonth = function()
                 {
-                    return "April"
+                    let today = new Date();
+                    let date = (today.getMonth()+1);
+                    switch(date)
+                    {
+                        case 1  :   return "January";
+                        case 2  :   return "February";
+                        case 3  :   return "March";
+                        case 4  :   return "April";
+                        case 5  :   return "May";
+                        case 6  :   return "June";
+                        case 7  :   return "July";
+                        case 8  :   return "August";
+                        case 9  :   return "September";
+                        case 10  :   return "October";
+                        case 11  :   return "November";
+                        case 12  :   return "December";
+                    }
                 };
 let getBatchInfo = function()
                     {

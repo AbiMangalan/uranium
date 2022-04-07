927f02f3e9e5b47a17e9c54bceb8b9b55124024a
AbiMangalan <86941986+AbiMangalan@users.noreply.github.com>
2022-04-07 23:38:50 +0530

    assignment/get-api

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,22 +2,76 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/user-profile/:abcd', function(req, res) {
-    console.log(req)
-    console.log(req.params.abcd)
-    res.send('dummy response')
-})
-
-router.get('/test-me', function (req, res) {
-    console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
+router.get('/movies', function(req, res) 
+{
+    let movies = ['The Shining','Incendies','Rang de Basanti','Finding Nemo'];
+    res.send(movies)
 });
 
+router.get('/movies/:muvIndex', function(req, res) 
+{
+    let movies = ['The Shining','Incendies','Rang de Basanti','Finding Nemo'];
+    if(req.params.muvIndex>movies.length-1)
+    {
+        res.send('Invalid Index!');
+    }
+    else
+    {
+        res.send(movies[req.params.muvIndex]);
+    }
+});
 
+router.get('/films', function (req, res) 
+{       
+    let films = [  
+        {
+            "id": 1,
+            'muvName': "The Shining"
+        }, 
+        {
+            "id": 2,
+            "name": "Incendies"
+        }, 
+        {
+            "id": 3,
+            "name": "Rang de Basanti"
+        }, 
+        {
+            "id": 4,
+            "name": "Finding Nemo"
+        }
+    ];
+    res.send(films);
+});
 
+router.get('/films/:id', function (req, res) 
+{
+    let films = [  
+        {
+            "id": 1,
+            'muvName': "The Shining"
+        }, 
+        {
+            "id": 2,
+            "name": "Incendies"
+        }, 
+        {
+            "id": 3,
+            "name": "Rang de Basanti"
+        }, 
+        {
+            "id": 4,
+            "name": "Finding Nemo"
+        }
+    ];
+    if(req.params.id>films.length)
+    {
+        res.send('No movie exists with this id!');
+    }
+    else
+    {
+        res.send(films.find(x=>x.id==req.params.id));
+    }
+});
 
 module.exports = router;
-// adding this comment for no reason
